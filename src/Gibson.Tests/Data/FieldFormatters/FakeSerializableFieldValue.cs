3fb158752e7ae029ab7b9f23c1d26ebb659cf080
Kam Figy <kamsar@gmail.com>
2015-05-17 23:17:36 -0700

    Formatter tests

---
+++ src/Gibson.Tests/Data/FieldFormatters/FakeSerializableFieldValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gibson.Model;
+
+namespace Gibson.Tests.Data.FieldFormatters
+{
+	public class FakeSerializableFieldValue : ISerializableFieldValue
+	{
+		public FakeSerializableFieldValue(string value, string fieldType = "Test", Guid fieldId = new Guid())
+		{
+			Value = value;
+			FieldType = fieldType;
+			FieldId = fieldId;
+		}
+
+		public Guid FieldId { get; private set; }
+
+		public string Value { get; private set; }
+
+		public string FieldType { get; private set; }
+	}
+}
