e6851b769288c3e119b9bc4ec36d7008097e391f
Kam Figy <kamsar@gmail.com>
2015-07-12 07:59:54 -0700

    Filtering tests

---
+++ src/Rainbow.Tests/Filtering/ConfigurationFieldFilterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml;
+using NUnit.Framework;
+using Rainbow.Filtering;
+
+namespace Rainbow.Tests.Filtering
+{
+	public class ConfigurationFieldFilterTests
+	{
+		[Test]
+		public void FieldFilter_IgnoresExpectedFields()
+		{
+			var configXml = new XmlDocument();
+			configXml.LoadXml(@"<fieldFilter><exclude fieldID=""{B1E16562-F3F9-4DDD-84CA-6E099950ECC0}"" note=""'Last run' field on Schedule template (used to register tasks)"" /></fieldFilter>");
+
+			var filter = new ConfigurationFieldFilter(configXml.DocumentElement);
+
+			Assert.IsFalse(filter.Includes(new Guid("{B1E16562-F3F9-4DDD-84CA-6E099950ECC0}")));
+		}
+
+		[Test]
+		public void FieldFilter_IncludesExpectedFields()
+		{
+			var configXml = new XmlDocument();
+			configXml.LoadXml(@"<fieldFilter><exclude fieldID=""{B1E16562-F3F9-4DDD-84CA-6E099950ECC0}"" note=""'Last run' field on Schedule template (used to register tasks)"" /></fieldFilter>");
+
+			var filter = new ConfigurationFieldFilter(configXml.DocumentElement);
+
+			Assert.IsTrue(filter.Includes(Guid.NewGuid()));
+		}
+	}
+}
---
+++ src/Rainbow.Tests/Filtering/FilteredItemTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Rainbow.Filtering;
+
+namespace Rainbow.Tests.Filtering
+{
+	public class FilteredItemTests
+	{
+		[Test]
+		public void FilteredItem_DoesNotContainExpectedSharedField()
+		{
+			Guid fieldId = Guid.NewGuid();
+
+			var filter = new TestFieldFilter(fieldId);
+			var testItem = new FakeItem(sharedFields: new[] { new FakeFieldValue("Fake", fieldId: fieldId), });
+
+			var filteredItem = new FilteredItem(testItem, filter);
+
+			Assert.IsFalse(filteredItem.SharedFields.Any(field => field.FieldId == fieldId));
+		}
+
+		[Test]
+		public void FilteredItem_ContainsExpectedSharedField()
+		{
+			Guid fieldId = Guid.NewGuid();
+
+			var filter = new TestFieldFilter();
+			var testItem = new FakeItem(sharedFields: new[] { new FakeFieldValue("Fake", fieldId: fieldId) });
+
+			var filteredItem = new FilteredItem(testItem, filter);
+
+			Assert.IsTrue(filteredItem.SharedFields.Any(field => field.FieldId == fieldId));
+		}
+
+		[Test]
+		public void FilteredItem_DoesNotContainExpectedVersionedField()
+		{
+			Guid fieldId = Guid.NewGuid();
+
+			var filter = new TestFieldFilter(fieldId);
+			var testItem = new FakeItem(versions: new[] { new FakeItemVersion(fields: new FakeFieldValue("Fake", fieldId: fieldId) )});
+
+			var filteredItem = new FilteredItem(testItem, filter);
+
+			Assert.IsFalse(filteredItem.Versions.First().Fields.Any(field => field.FieldId == fieldId));
+		}
+
+		[Test]
+		public void FilteredItem_ContainsExpectedVersionedField()
+		{
+			Guid fieldId = Guid.NewGuid();
+
+			var filter = new TestFieldFilter();
+			var testItem = new FakeItem(versions: new[] { new FakeItemVersion(fields: new FakeFieldValue("Fake", fieldId: fieldId)) });
+
+			var filteredItem = new FilteredItem(testItem, filter);
+
+			Assert.IsTrue(filteredItem.Versions.First().Fields.Any(field => field.FieldId == fieldId));
+		}
+
+		private class TestFieldFilter : IFieldFilter
+		{
+			private readonly HashSet<Guid> _fields;
+			public TestFieldFilter(params Guid[] excludedFieldIds)
+			{
+				_fields = new HashSet<Guid>(excludedFieldIds);
+			}
+
+			public bool Includes(Guid fieldId)
+			{
+				return !_fields.Contains(fieldId);
+			}
+		}
+	}
+}
