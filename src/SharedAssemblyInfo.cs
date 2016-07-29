2092760fc730218ef63c2dc76bf4882a35067312
Kam Figy <kamsar@gmail.com>
2016-07-29 14:36:58 -0700

    1.4RC2

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.0-RC01")]
+[assembly: AssemblyInformationalVersion("1.4.0-RC02")]
 [assembly: CLSCompliant(false)]
