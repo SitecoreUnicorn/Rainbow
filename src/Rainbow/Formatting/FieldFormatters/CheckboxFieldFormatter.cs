1be68e3c72ddd972817523759944c5d0e93e22b0
Mark Cassidy <markc@corepoint-it.net>
2016-08-16 19:39:02 +0200

    Fix misbehaving checkboxes and superfluous standard values handling

--- src/Rainbow/Formatting/FieldFormatters/CheckboxFieldFormatter.cs
+++ src/Rainbow/Formatting/FieldFormatters/CheckboxFieldFormatter.cs
@@ -4,16 +4,15 @@ using Rainbow.Model;
 namespace Rainbow.Formatting.FieldFormatters
 {
 	/// <summary>
-	/// Formats checkboxes as explicit values of "0" or "1"
-	/// Normally Sitecore stores false as a blank value. This is bad because it can cause havoc with blanks 
-	/// versus standard values. So to disambiguate this, we force false to zero.
-	/// 
-	/// This DOES mean that a sync after reserializing checkbox fields will detect a 'change' as the value
-	/// in Sitecore is normalized to zero.
+	///     Formats checkboxes as explicit values of "0" or "1"
+	///     Normally Sitecore stores false as a blank value. This is bad because it can cause havoc with blanks
+	///     versus standard values. So to disambiguate this, we force false to zero.
+	///     This DOES mean that a sync after reserializing checkbox fields will detect a 'change' as the value
+	///     in Sitecore is normalized to zero.
 	/// </summary>
 	public class CheckboxFieldFormatter : FieldTypeBasedFormatter
 	{
-		public override string[] SupportedFieldTypes => new[] { "Checkbox" };
+		public override string[] SupportedFieldTypes => new[] {"Checkbox"};
 
 		public override string Format(IItemFieldValue field)
 		{
@@ -24,6 +23,8 @@ namespace Rainbow.Formatting.FieldFormatters
 
 		public override string Unformat(string value)
 		{
+			if (value.Equals("0", StringComparison.Ordinal))
+				return string.Empty;
 			return value;
 		}
 	}
