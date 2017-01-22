0eb0775ac41a9f28f043d0a5013d69afac2c3499
Kam Figy <kamsar@gmail.com>
2017-01-21 23:51:02 -0800

    Debuggah enhancement :)

--- src/Rainbow/Storage/TreeRoot.cs
+++ src/Rainbow/Storage/TreeRoot.cs
@@ -1,8 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Rainbow.Storage
 {
 	[ExcludeFromCodeCoverage]
+	[DebuggerDisplay("{Name} {DatabaseName}:{Path}")]
 	public class TreeRoot
 	{
 		public TreeRoot(string name, string path, string databaseName)
