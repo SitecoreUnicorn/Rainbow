785421deb815b5a2b0216ad0b5326ec19657d089
Kam Figy <kamsar@gmail.com>
2017-01-29 22:39:26 -0800

    Get rid of FastDirectoryEnumerator, as it was causing file locking errors on reserialization.
    Must be some sort of unmanaged resource disposal - it has a dispose on it, but it must be incomplete. Either way perf does not seem majorly impacted especially with DiSerialized.

--- src/Rainbow/Storage/SerializationFileSystemTree.cs
+++ src/Rainbow/Storage/SerializationFileSystemTree.cs
@@ -550,21 +550,21 @@ namespace Rainbow.Storage
 			if (serializedItem == null)
 				throw new InvalidOperationException("Item {0} does not exist on disk.".FormatWith(item.Path));
 
-			IEnumerable<FileData> children = Enumerable.Empty<FileData>();
+			IEnumerable<string> children = Enumerable.Empty<string>();
 
 			var childrenPath = Path.ChangeExtension(serializedItem.SerializedItemId, null);
 
 			if (Directory.Exists(childrenPath))
 			{
-				children = FastDirectoryEnumerator.GetFiles(childrenPath, "*" + _formatter.FileExtension, SearchOption.TopDirectoryOnly);
+				children = Directory.EnumerateFiles(childrenPath, "*" + _formatter.FileExtension, SearchOption.TopDirectoryOnly);
 			}
 
 			var shortPath = Path.Combine(_physicalRootPath, item.Id.ToString());
 
 			if (Directory.Exists(shortPath))
-				children = children.Concat(FastDirectoryEnumerator.GetFiles(shortPath, "*" + _formatter.FileExtension, SearchOption.TopDirectoryOnly));
+				children = children.Concat(Directory.EnumerateFiles(shortPath, "*" + _formatter.FileExtension, SearchOption.TopDirectoryOnly));
 
-			return children.Select(result => result.Path).ToArray();
+			return children.ToArray();
 		}
 
 		protected virtual string PrepareItemNameForFileSystem(string name)
@@ -616,7 +616,7 @@ namespace Rainbow.Storage
 
 			var result = GetPhysicalFilePathsForVirtualPath(localPath)
 				.Select(ReadItemMetadata)
-				.FirstOrDefault(candidateItem => candidateItem.Id == expectedItemId);
+				.FirstOrDefault(candidateItem => candidateItem != null && candidateItem.Id == expectedItemId);
 
 			if (result == null) return null;
 
