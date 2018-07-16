2b64a227499a4267494c3b77ba86580f51cba88a
Mark Cassidy <mark@cassidy.dk>
2018-07-16 22:59:47 +0200

    Minor housekeeping. Updated project URLs, authors

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Connective DX")]
 [assembly: AssemblyProduct("Rainbow")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.0.1.0")]
