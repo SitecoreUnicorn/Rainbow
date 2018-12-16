55af6ab102e777ba150529dc044c001ad0016f29
cassidydotdk <mark@cassidy.dk>
2018-12-16 14:40:50 +0100

    Ticked to version 2.0.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Rainbow")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.3.0")]
-[assembly: AssemblyFileVersion("2.0.3.0")]
-[assembly: AssemblyInformationalVersion("2.0.3-pre2")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
+[assembly: AssemblyInformationalVersion("2.0.4")]
 [assembly: CLSCompliant(false)]
