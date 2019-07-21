40d5dd73148cba33a83e0700a431577362f7eaf9
Mark Cassidy <mark@cassidy.dk>
2019-07-22 00:36:07 +0200

    Ticked version to 2.1.1

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.1.1.0")]
 [assembly: AssemblyFileVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.1-pre1")]
+[assembly: AssemblyInformationalVersion("2.1.1")]
 [assembly: CLSCompliant(false)]
