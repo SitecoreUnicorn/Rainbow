3366c917df58fe0f7e6ca6d1a3b0cb267dd4c7f4
Mark Cassidy <mark@cassidy.dk>
2021-06-14 17:45:21 +0200

    Ticked version to 2.1.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.1.4.0")]
 [assembly: AssemblyFileVersion("2.1.4.0")]
-[assembly: AssemblyInformationalVersion("2.1.4-pre3")]
+[assembly: AssemblyInformationalVersion("2.1.4")]
 [assembly: CLSCompliant(false)]
